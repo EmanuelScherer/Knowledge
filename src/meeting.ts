66a2f2f92fde5c62dc8ad36f4b692ede92f63a09
EmanuelScherer <emanuel.scherer@meta.com.br>
2020-04-27 11:55:37 -0300

    .

--- src/meeting.ts
+++ src/meeting.ts
@@ -1,7 +1,9 @@
 import * as fs from 'fs';
 import * as electron from 'electron';
 import { v4 as uuidv4 } from 'uuid';
-import axios from 'axios'
+import axios from 'axios';
+
+const ProgressBar = require('../src/electron-progressbar/source/index')
 
 const con = electron.remote.getGlobal('console')
 
@@ -93,6 +95,17 @@ const div_tarefas: HTMLDivElement | null = document.querySelector("div#tarefas")
 const div_tarefas2: HTMLDivElement | null = document.querySelector("div#d_tf")
 
 const bt_add: NodeListOf<HTMLButtonElement> | null = document.querySelectorAll("button#bt_add")
+
+interface ObjTrello {
+
+    Card_Id: string
+
+    Descricao: string
+
+    Impedida: boolean
+    Impedimento: string
+
+}
 
 class Trello {
 
@@ -190,7 +203,7 @@ class Trello {
                                    
                                         case "To Do": case "Doing":
 
-                                            AddNode(res[c].name, id, false);
+                                            AddNode(res[c].name, res[c].id, false);
                                             break;
 
                                         case "Done":
@@ -199,7 +212,7 @@ class Trello {
 
                                         case "Blocked":
 
-                                            AddNode(res[c].name, id, true, res[c].desc.replace("Impedimento:", ""));
+                                            AddNode(res[c].name, res[c].id, true, res[c].desc.replace("Impedimento:", ""));
                                             break;
 
                                     }
@@ -222,9 +235,169 @@ class Trello {
 
     }
 
-    PostTrello = (Doto: object[], Doing: object[], Blocked: object[], Done: object[]) => {
+    PostTrello = async (Todo: ObjTrello[], Doing: ObjTrello[], Blocked: ObjTrello[], Done: ObjTrello[]) => {
 
+        console.log("post trello")
 
+        let LTodo = ""
+        let LDoing = ""
+        let LDone = ""
+        let LBlocked = ""
+
+        for (let cf in configs) {
+
+            for (let t in configs[cf].teams) {
+
+                if (configs[cf].teams[t].name == select_team?.value) {
+
+                    for (let l in configs[cf].teams[t].trello.lists) {
+
+                        if (configs[cf].teams[t].trello.lists[l].name == "To Do") {
+
+                            LTodo = configs[cf].teams[t].trello.lists[l].id
+
+                        }
+
+                        if (configs[cf].teams[t].trello.lists[l].name == "Doing") {
+
+                            LDoing = configs[cf].teams[t].trello.lists[l].id
+
+                        }
+
+                        if (configs[cf].teams[t].trello.lists[l].name == "Blocked") {
+
+                            LBlocked = configs[cf].teams[t].trello.lists[l].id
+
+                        }
+
+                        if (configs[cf].teams[t].trello.lists[l].name == "Done") {
+
+                            LDone = configs[cf].teams[t].trello.lists[l].id
+
+                        }
+
+                    }
+
+                }
+
+            }
+
+        }
+
+        console.log("posting: "+JSON.stringify(Todo)+"\n\n"+JSON.stringify(Doing)+"\n\n"+JSON.stringify(Blocked)+"\n\n"+JSON.stringify(Done))
+
+        if (Todo[0] != undefined) {
+
+            for (let i in Todo) {
+
+                console.log("posting Todo")
+
+                await axios.put("https://api.trello.com/1/cards/"+Todo[i].Card_Id+"?idList="+LTodo+"&key="+this.key+"&token="+this.token)
+                .then(r => {
+
+                    alert("deu")
+
+                })
+                .catch(e => {
+
+                    alert("deu erro: "+e)
+
+                })
+
+            }
+
+        }
+
+        if (Doing[0] != undefined) {
+
+            for (let i in Doing) {
+
+                console.log("posting Doing")
+
+                await axios.put("https://api.trello.com/1/cards/"+Doing[i].Card_Id+"?idList="+LDoing+"&key="+this.key+"&token="+this.token)
+                .then(r => {
+
+                    alert("deu")
+
+                })
+                .catch(e => {
+
+                    alert("deu erro: "+e)
+
+                })
+
+            }
+
+        }
+
+        if (Blocked[0] != undefined) {
+
+            for (let i in Blocked) {
+
+                console.log("posting Blocked")
+
+                await axios.put("https://api.trello.com/1/cards/"+Blocked[i].Card_Id+"?idList="+LBlocked+"&key="+this.key+"&token="+this.token)
+                .then(r => {
+
+                    alert("deu")
+
+                })
+                .catch(e => {
+
+                    alert("deu erro: "+e)
+
+                })
+
+            }
+
+        }
+
+        if (Done[0] != undefined) {
+
+            for (let i in Done) {
+
+                console.log("posting Done")
+
+                await axios.put("https://api.trello.com/1/cards/"+Done[i].Card_Id+"?idList="+LDone+"&key="+this.key+"&token="+this.token)
+                .then(r => {
+
+                    alert("deu")
+
+                })
+                .catch(e => {
+
+                    alert("deu erro: "+e)
+
+                })
+
+            }
+
+        }
+
+        let progressBar = new ProgressBar({
+            text: 'Preparing data...',
+            detail: 'Wait...'
+        });
+          
+          progressBar
+            .on('completed', function() {
+              console.info(`completed...`);
+              progressBar.detail = 'Task completed. Exiting...';
+            })
+            .on('aborted', function() {
+              console.info(`aborted...`);
+            });
+          
+          // launch a task...
+          // launchTask();
+          
+          // when task is completed, set the progress bar to completed
+          // ps: setTimeout is used here just to simulate an interval between the start and the end of a task
+          setTimeout(function() {
+            progressBar.setCompleted();
+          }, 3000);
+
+        console.log("posting concluido")
 
     }
 
@@ -315,24 +488,28 @@ const AddNode = (tarefa: string, id: string, impedida: boolean, impedimento?: st
 
         HTML = `
     
-            <div style="background-color: #22323f; id="tf">
+            <div style="background-color: #22323f;" id="tf_`+id+`">
 
-                <div style="padding: 10px; margin-top: 10px; margin-bottom: 10px;" id="`+id+`">
+                <div style="background-color: #22323f; id="tf_`+id+`">
 
-                    <textarea id="text_tarefa_`+id+`" style="background-color: #2b3f4e; text-align: center;" placeholder="Expecifique a tarefa..." required>`+tarefa+`</textarea>
+                    <div style="padding: 10px; margin-top: 10px; margin-bottom: 10px;" id="`+id+`">
 
-                    <div class="select-wrapper" id="select_cinza" style="margin-top: 10px;">
+                        <textarea id="text_tarefa_`+id+`" style="background-color: #2b3f4e; text-align: center;" placeholder="Expecifique a tarefa..." required>`+tarefa+`</textarea>
 
-                        <select id="select_status_`+id+`" style="color: white; background-color: #2b3f4e;">
+                        <div class="select-wrapper" id="select_cinza" style="margin-top: 10px;">
 
-                            <option>Vou fazer hoje</option>
-                            <option>Não Vou fazer hoje</option>
-                            <option>Concluido</option>
-                            <option selected>Impedido</option>
+                            <select id="select_status_`+id+`" style="color: white; background-color: #2b3f4e;">
 
-                        </select>
+                                <option>Vou fazer hoje</option>
+                                <option>Não Vou fazer hoje</option>
+                                <option>Concluido</option>
+                                <option selected>Impedido</option>
 
-                        <textarea id="text_impedimento_`+id+`" style="background-color: #2b3f4e; text-align: center; margin-top: 10px;" placeholder="Expecifique o impedimento..." required>`+impedimento+`</textarea>
+                            </select>
+
+                            <textarea id="text_impedimento_`+id+`" style="background-color: #2b3f4e; text-align: center; margin-top: 10px;" placeholder="Expecifique o impedimento..." required>`+impedimento+`</textarea>
+
+                        </div>
 
                     </div>
 
@@ -346,9 +523,9 @@ const AddNode = (tarefa: string, id: string, impedida: boolean, impedimento?: st
     else {
 
         HTML = `
-    
-            <div style="background-color: #22323f; id="tf">
 
+            <div style="background-color: #22323f;" id="tf_`+id+`">
+    
                 <div style="padding: 10px; margin-top: 10px; margin-bottom: 10px;" id="`+id+`">
 
                     <textarea id="text_tarefa_`+id+`" style="background-color: #2b3f4e; text-align: center;" placeholder="Expecifique a tarefa..." required>`+tarefa+`</textarea>
@@ -410,7 +587,7 @@ bt_add?.forEach(b => {
     
         let HTML = `
         
-            <div style="background-color: #22323f; id="tf">
+            <div style="background-color: #22323f;" id="tf_`+id+`">
     
                 <div style="padding: 10px; margin-top: 10px; margin-bottom: 10px;" id="`+id+`">
     
@@ -576,7 +753,7 @@ select_team?.addEventListener('change', (e) => {
 
 })
 
-from_team?.addEventListener('submit', (e) => {
+from_team?.addEventListener('submit', async (e) => {
 
     e.preventDefault()
 
@@ -584,6 +761,59 @@ from_team?.addEventListener('submit', (e) => {
 
     const div_tf: HTMLDivElement | null = document.querySelector("div#d_tf")
 
+    let Todo: ObjTrello[] = []
+    let Doing: ObjTrello[] = []
+    let Done: ObjTrello[] = []
+    let Bloked: ObjTrello[] = []
 
+    if (div_tf?.children.length != undefined) {
+
+        for (let i = 0; i < div_tf?.children.length; i++) {
+
+            const id = div_tf?.children[i].id.replace("tf_", "")
+
+            const text: HTMLTextAreaElement | null = div_tf?.children[i].querySelector("textarea#text_tarefa_"+id)
+            const select: HTMLSelectElement | null = div_tf?.children[i].querySelector("select#select_status_"+id)
+            const imp: HTMLSelectElement | null = div_tf?.children[i].querySelector("textarea#text_impedimento_"+id)
+
+            const desc = text?.value
+
+            switch (select?.value) {
+
+                case "Vou fazer hoje": {
+
+                    Doing.push({Card_Id: id, Descricao: desc || "", Impedida: false, Impedimento: ""})
+                    break
+
+                }
+
+                case "Não Vou fazer hoje": {
+
+                    Todo.push({Card_Id: id, Descricao: desc || "", Impedida: false, Impedimento: ""})
+                    break
+
+                }
+
+                case "Concluido": {
+
+                    Done.push({Card_Id: id, Descricao: desc || "", Impedida: false, Impedimento: ""})
+                    break
+
+                }
+
+                case "Impedido": {
+
+                    Bloked.push({Card_Id: id, Descricao: desc || "", Impedida: true, Impedimento: imp?.value || ""})
+                    break
+
+                }
+
+            }
+
+        }
+
+        await tr.PostTrello(Todo, Doing, Bloked, Done)
+
+    }
 
 })
