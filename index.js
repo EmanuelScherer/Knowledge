b5ddaac71d6a68354371f3943d8cf67e29f18acf
manoifsul <49045172+manoifsul@users.noreply.github.com>
2020-06-16 11:36:30 -0300

    .

--- index.js
+++
@@ -1,3 +0,0 @@
-const index = require('./BUILD/src/index')
-
-//const test = require("./tests/TesteElectron")
--- tests/a.js
+++
@@ -1,9 +0,0 @@
-const con = require('electron').remote.getGlobal('console')
-
-const bt = document.getElementById('bt')
-
-    bt.addEventListener('click', () => {
-
-        con.log('aaaaaaaaaaaaaa')
-
-    })
